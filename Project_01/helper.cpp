c301fad3091d7b28d0eb789565856b08b63261d9
Dương Trọng Nghĩa <nghiaduong.170697@outlook.com.vn>
2017-10-18 17:08:01 +0700

    Fix bug

--- Project_01/helper.cpp
+++ Project_01/helper.cpp
@@ -38,16 +38,24 @@ void sort_list(vector<Node*> &node_list, vector<int> heuristic_value, bool is_in
 		{
 			if (is_include_cost)
 			{
-				if (heuristic_value.at(node_list.at(i)->value) + node_list.at(i)->cost > heuristic_value.at(node_list.at(j)->value) + node_list.at(j)->cost)
+				if (heuristic_value.at(node_list.at(i)->value) + node_list.at(i)->cost >= heuristic_value.at(node_list.at(j)->value) + node_list.at(j)->cost)
 				{
-					swap(node_list[i], node_list[j]);
+					if(heuristic_value.at(node_list.at(i)->value) + node_list.at(i)->cost == heuristic_value.at(node_list.at(j)->value) + node_list.at(j)->cost)
+						if(node_list[i]->value > node_list[j]->value)
+							swap(node_list[i], node_list[j]);
 				}
+				else
+					swap(node_list[i], node_list[j]);
 			}
 			else
 			{
-				if (heuristic_value.at(node_list.at(i)->value) > heuristic_value.at(node_list.at(j)->value))
+				if (heuristic_value.at(node_list.at(i)->value) >= heuristic_value.at(node_list.at(j)->value))
 				{
-					swap(node_list[i], node_list[j]);
+					if (heuristic_value.at(node_list.at(i)->value) == heuristic_value.at(node_list.at(j)->value))
+						if (node_list[i]->value > node_list[j]->value)
+							swap(node_list[i], node_list[j]);
+						else
+							swap(node_list[i], node_list[j]);
 				}
 			}
 		}
